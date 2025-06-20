6036d601af72b252d52af6c02fbea600da927468
Rune Molander <53047624+Itszipzon@users.noreply.github.com>
2025-06-21 01:49:35 +0200

    Added github and linkedin links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import "./css/Home.css";
+import { Link } from "react-router";
+import githubLogo from "../assets/logos/github.svg";
+import linkedInLogo from "../assets/logos/linkedin.png";
 
 export default function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of our application.</p>
+      <div className="home-social-links">
+        <Link to="https://github.com/Itszipzon" target="_blank" rel="noopener noreferrer" ><img src={githubLogo} alt="GitHub Logo" className="github-logo" /></Link>
+        <Link to="https://www.linkedin.com/in/rune-molander-a943a0263/" target="_blank" rel="noopener noreferrer"><img src={linkedInLogo} alt="LinkedIn Logo" className="linkedin-logo" /></Link>
+      </div>
     </div>
   );
 }
