b7c35e930aeeb55025901367c2e0175422b02368
Rune Molander <53047624+Itszipzon@users.noreply.github.com>
2025-06-23 02:54:51 +0200

    Added placeholder image

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -7,7 +7,7 @@ export default function Apps() {
     <div className="apps">
       <h1>Apps</h1>
       <ul>
-        <li><AppContainer title="Locked In" img="https://shelstad.store/data/tool2.png" /></li>
+        <li><AppContainer title="Locked In" img="https://placeholdit.com/150x150/dddddd/999999?font=roboto" /></li>
         <li><Link to="/app2">App 2</Link></li>
         <li><Link to="/app3">App 3</Link></li>
       </ul>
